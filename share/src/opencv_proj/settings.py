fced690e8b6767ce23d1fa51d0668f334144fc9e
vineek28 <108052599+vineek28@users.noreply.github.com>
2023-04-18 10:14:01 +0530

    updated the static urls

--- share/src/opencv_proj/settings.py
+++ share/src/opencv_proj/settings.py
@@ -132,6 +132,8 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
